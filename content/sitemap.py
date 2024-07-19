9143b02ea21338633edf7521d028ab0546903d7a
Nitin Gautam <gautamn2002@gmail.com>
2024-07-19 16:44:26 +0530

    changes as per plugins

--- content/sitemap.py
+++ content/sitemap.py
@@ -7,14 +7,14 @@ from thirdparty.openai_assistant import AssistantAPI
 
 logger = get_logger(__name__)
 
-instructions = '''
+instructions = """
  
 You are an SEO Expert. Your task is generate sitemap.xml for www.rgbkit.com.
 You SEO file should be a vaild xml and should follow all the norms so that Search Engines can accept it. 
 
-'''
+"""
 
-prompt = '''
+prompt = """
 Please help me in creating sitemap.xml that I will use for SEO purpose. You need to pick the urls from the given 
 text below and add url in loc tag and current timestamp in lastmod tag. The format of the tag would be '2024-06-26T11:55:48+00:00'. 
 
@@ -34,57 +34,56 @@ Make ensure that only xml response is returned in the output and no text is incl
 
 CONTENT: {content}
 
-'''
+"""
 
 client = AssistantAPI.create_client(
-            api_key="42ef311b5f274981be65da45d46bac65",
-            api_version="2024-02-15-preview",
-            azure_endpoint="https://njcswedencentral.openai.azure.com/"
-        )
+    api_key="42ef311b5f274981be65da45d46bac65",
+    api_version="2024-02-15-preview",
+    azure_endpoint="https://njcswedencentral.openai.azure.com/",
+)
 
 assistant = client.beta.assistants.create(
-                name="SEO Expert",
-                instructions=instructions,
-                model="njc-assistant-gpt4-32k"
-            )
+    name="SEO Expert", instructions=instructions, model="njc-assistant-gpt4-32k"
+)
 
-valid_urls_filename="valid_urls.txt"
+valid_urls_filename = "valid_urls.txt"
 output_sitemap_filename = "new_sitemap.xml"
 
+
 def create_sitemap_file(new_urls_file_path):
-    
+
     # Get the directory name
-    directory = os.path.dirname(new_urls_file_path)    
+    directory = os.path.dirname(new_urls_file_path)
     print(f"directory={directory}")
 
-    #Exclude page with 404 error
+    # Exclude page with 404 error
     valid_urls_file_path = os.path.join(directory, valid_urls_filename)
 
     exclude_invalid_urls(new_urls_file_path, valid_urls_file_path)
-    
-    with open(valid_urls_file_path, 'r', encoding='utf-8') as file:
+
+    with open(valid_urls_file_path, "r", encoding="utf-8") as file:
         content = file.read()
 
-    #Generate sitemap
-    sitemap=generate_sitemap(content)    
+    # Generate sitemap
+    sitemap = generate_sitemap(content)
 
     # Save the response
     output_sitemap_file_path = os.path.join(directory, output_sitemap_filename)
-    save_content(output_sitemap_file_path , sitemap)
+    save_content(output_sitemap_file_path, sitemap)
+
 
 def generate_sitemap(content):
     print("Read Entire File:\n", content)
-    str=prompt.replace("{content}", content)
+    str = prompt.replace("{content}", content)
     assistant_api = AssistantAPI(client)
     response = assistant_api.execute_thread_with_content(
-        content=str, 
-        assistant=assistant,
-        thread=assistant_api.create_thread())
-    return response 
+        content=str, assistant=assistant, thread=assistant_api.create_thread()
+    )
+    return response
 
-def save_content(file_path, content):   
+def save_content(file_path, content):
     # Write content to the file
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
 
     print(f"File created and content saved at: {file_path}")
@@ -92,12 +91,13 @@ def save_content(file_path, content):
 
 def exclude_invalid_urls(new_urls_file_path, valid_urls_filepath):
     """Reads URLs from a file."""
-    with open(new_urls_file_path, 'r') as file:
+    with open(new_urls_file_path, "r") as file:
         urls = file.readlines()
     urls = [url.strip() for url in urls]
     valid_urls = [url for url in urls if check_url(url)]
 
     write_valid_urls(valid_urls, valid_urls_filepath)
+
 
 def check_url(url):
     """Checks if a URL returns a 404 status code."""
@@ -110,9 +110,10 @@ def check_url(url):
     except requests.RequestException as e:
         print(f"Error checking {url}: {e}")
         return False
-    
+
+
 def write_valid_urls(urls, output_file_path):
     """Writes valid URLs to a new file."""
-    with open(output_file_path, 'w') as file:
+    with open(output_file_path, "w") as file:
         for url in urls:
-            file.write(url + '\n')               
+            file.write(url + "\n")
--- main.py
+++ main.py
@@ -9,51 +9,46 @@ from logger import get_logger
 logger = get_logger(__name__)
 
 if __name__ == "__main__":
-  
-   logger.info("Running SEO Writer for basic file operations!")
-   
-   if len(sys.argv) <= 1:
-          logger.info("Invalid Commands! No utility name provided in the given command")
-          logger.info("Usage: python main.py <UTILITY NAME> [PARAMS]")
-          logger.info("Exiting the program...")
-          sys.exit()
-   else:
-         utility_name = sys.argv[1] 
-         # python main.py SEO_CONTENT "Crop Image" 
-         if utility_name == 'SEO_CONTENT':
-              if len(sys.argv) == 2:
-                     logger.info("utility name or feature name is missing.")
-                     logger.info("Usage: python main.py SEO_CONTENT <feature name in quotes>")
-                     sys.exit()
-              create_seo_content(sys.argv[2])
 
-         # python main.py TRANSLATE "C:/my-workspace/seo-content/gif-maker.json"    
-         elif utility_name == 'TRANSLATE':
-              if len(sys.argv) == 2:
-                     logger.info("utility name or feature name is missing.")
-                     logger.info("Usage: python main.py TRANSLATE <absolute file path>")
-                     sys.exit()
-              translate_file_content(sys.argv[2])
+    logger.info("Running SEO Writer for basic file operations!")
 
-         # python main.py WRITE_BLOG "Remove Bankground"      
-         elif utility_name == 'WRITE_BLOG':
-              if len(sys.argv) == 2:
-                     logger.info("utility name or feature name is missing.")
-                     logger.info("Usage: python main.py WRITE_BLOG <feature name in quotes>")
-                     sys.exit()
-              write_blog(sys.argv[2]) 
-        
-         # python main.py GEN_SITEMAP "I:/My Drive/rgbkit-sharedcontent/sitemap/new_urls.txt"      
-         elif utility_name == 'GEN_SITEMAP':
-              if len(sys.argv) == 2:
-                     logger.info("utility name or feature name is missing.")
-                     logger.info("Usage: python main.py GEN_SITEMAP <absolute file path>")
-                     sys.exit()
-              create_sitemap_file(sys.argv[2])               
+    if len(sys.argv) <= 1:
+        logger.info("Invalid Commands! No utility name provided in the given command")
+        logger.info("Usage: python main.py <UTILITY NAME> [PARAMS]")
+        logger.info("Exiting the program...")
+        sys.exit()
+    else:
+        utility_name = sys.argv[1]
+        # python main.py SEO_CONTENT "Crop Image"
+        if utility_name == "SEO_CONTENT":
+            if len(sys.argv) == 2:
+                logger.info("utility name or feature name is missing.")
+                logger.info(
+                    "Usage: python main.py SEO_CONTENT <feature name in quotes>"
+                )
+                sys.exit()
+            create_seo_content(sys.argv[2])
 
+        # python main.py TRANSLATE "C:/my-workspace/seo-content/gif-maker.json"
+        elif utility_name == "TRANSLATE":
+            if len(sys.argv) == 2:
+                logger.info("utility name or feature name is missing.")
+                logger.info("Usage: python main.py TRANSLATE <absolute file path>")
+                sys.exit()
+            translate_file_content(sys.argv[2])
 
-       
-         
-             
+        # python main.py WRITE_BLOG "Remove Bankground"
+        elif utility_name == "WRITE_BLOG":
+            if len(sys.argv) == 2:
+                logger.info("utility name or feature name is missing.")
+                logger.info("Usage: python main.py WRITE_BLOG <feature name in quotes>")
+                sys.exit()
+            write_blog(sys.argv[2])
 
-
+        # python main.py GEN_SITEMAP "I:/My Drive/rgbkit-sharedcontent/sitemap/new_urls.txt"
+        elif utility_name == "GEN_SITEMAP":
+            if len(sys.argv) == 2:
+                logger.info("utility name or feature name is missing.")
+                logger.info("Usage: python main.py GEN_SITEMAP <absolute file path>")
+                sys.exit()
+            create_sitemap_file(sys.argv[2])
